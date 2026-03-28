a5e36726bbe0bf9c3d63f1f5c889e2c4c46b737a
liviadfsilva <liviadfsilva@gmail.com>
2026-03-28 11:03:57 -0300

    feat: ask user for position

--- app/src/main/java/tictactoe/sertech/App.java
+++ app/src/main/java/tictactoe/sertech/App.java
@@ -1,10 +1,14 @@
 package tictactoe.sertech;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         
         // Board
-        String[][] board = { {" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
+        char[][] board = { {' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
 
         System.out.println("Welcome to Tic-Tac-Toe!");
         System.out.println();
@@ -16,5 +20,30 @@ public class App {
             System.out.println();
             System.out.println();
         }
+
+        // Game on
+        boolean game = true;
+        while(game){
+            System.out.println("Type a position: ");
+            System.out.println();
+
+            int lineChosen = scanner.nextInt();
+            int rowChosen = scanner.nextInt();
+
+            board[lineChosen][rowChosen] = 'X';
+
+            System.out.println();
+
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    System.out.print("[ " + board[i][j] + " ] ");
+                }
+            System.out.println();
+            System.out.println();
+            }
+        }
+
+        scanner.close();
+
     }
 }

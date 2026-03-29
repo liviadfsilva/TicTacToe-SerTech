abec423a3ca72f75a670e6a218cb61fa27e8001e
liviadfsilva <liviadfsilva@gmail.com>
2026-03-29 17:42:48 -0300

    feat: add victory check

--- app/src/main/java/tictactoe/sertech/App.java
+++ app/src/main/java/tictactoe/sertech/App.java
@@ -51,12 +51,49 @@ public class App {
                 for (int j = 0; j < board[i].length; j++) {
                     System.out.print("[ " + board[i][j] + " ] ");
                 }
+
             System.out.println();
             System.out.println();
             }
+
+            // Victory Check
+            boolean hasWinner = false;
+            boolean hasEmpty = false;
+
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
+                    System.out.println(board[i][0] + " wins!");
+                    hasWinner = true;
+                }
+                if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
+                    System.out.println(board[0][i] + " wins!");
+                    hasWinner = true;
+                }
+            }
+
+            if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+                System.out.println(board[0][0] + " wins!");
+                hasWinner = true;
+            }
+
+            if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+                System.out.println(board[0][2] + " wins!");
+                hasWinner = true;
+            }
+
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board.length; j++) {
+                    if (board[i][j] == ' ') {
+                        hasEmpty = true;
+                    }
+                }
+            }
+
+            if (!hasWinner && !hasEmpty) {
+                System.out.println("Stalemate.");
+            }
+                    
         }
-
         scanner.close();
-
     }
 }
